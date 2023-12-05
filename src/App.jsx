a207b0c2870186136a1c817e398d3869ff10766e
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2023-12-05 11:01:37 +0000

    Feat: updated state of inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import Player from "./components/Player";
 
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id='game-container'>
+        <ol id='players'>
+          <Player name='Player 1' symbol='X'/>
+          <Player name='Player 2' symbol='O'/>
+        </ol>
+        Game board
+      </div>
+
+      <div>
+        LOG
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
